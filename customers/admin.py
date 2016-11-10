bd9218a5e3b65491ae5b80a1635619cbc33429ce
venkateswara reddy kasana <eswar0077@gmail.com>
2016-11-11 02:43:25 +0530

    added some more features

--- customers/admin.py
+++ customers/admin.py
@@ -9,5 +9,7 @@ class CustomerAdmin(admin.ModelAdmin):
 		('personal', {'fields':['first_name','last_name','contact_number']}),
 		('Address', {'fields':['business_name','street_name','town','district','pin_code']})
 	]
-
+	list_display = ('first_name','last_name','contact_number','business_name','town')
+	search_fields = ['first_name','business_name','contact_number']
+	list_filter = ['town']
 admin.site.register(Customer, CustomerAdmin)
